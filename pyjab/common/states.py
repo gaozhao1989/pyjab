ed60192d15cba05f52eed9bc401b55e08e1c7d90
Gary Gao <gaozhao89@qq.com>
2022-06-21 10:57:56 +0800

    update states

--- pyjab/common/states.py
+++ pyjab/common/states.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from tkinter import HORIZONTAL, VERTICAL
 
 """
 The States implementation.
@@ -10,19 +11,26 @@ class States(str, Enum):
     Set of supported states strategies.
     """
 
+    ACTIVE = "active"
     BUSY = "busy"
     CHECKED = "checked"
-    ENABLED = "enabled"
-    FOCUSED = "focused"
-    SELECTED = "selected"
-    PRESSED = "pressed"
-    EXPANDED = "expanded"
     COLLAPSED = "collapsed"
+    EDITABLE = "editable"
+    ENABLED = "enabled"
+    EXPANDED = "expanded"
+    FOCUSABLE = "focusable"
+    FOCUSED = "focused"
+    HORIZONTAL = "horizontal"
     ICONIFIED = "iconified"
     MODAL = "modal"
-    MULTI_LINE = "multi_line"
-    FOCUSABLE = "focusable"
-    EDITABLE = "editable"
-    VISIBLE = "visible"
+    MULTI_LINE = "multi line"
+    OPAQUE = "opaque"
+    RESIZEABLE = "resizeable"
+    SELECTABLE = "selectable"
+    SELECTED = "selected"
     SHOWING = "showing"
+    SIGNLE_LINE = "single line"
+    PRESSED = "pressed"
     UNKNOWN = "unknown"
+    VERTICAL = "vertical"
+    VISIBLE = "visible"
