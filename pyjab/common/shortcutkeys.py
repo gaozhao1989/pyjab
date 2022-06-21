56656531a7b625e0aedc1a993dfe38d03de7aff7
Gary Gao <gaozhao89@qq.com>
2022-06-21 14:32:39 +0800

    add generic shorcut keys for windows

--- pyjab/common/shortcutkeys.py
+++ pyjab/common/shortcutkeys.py
@@ -3,162 +3,115 @@ from pyjab.common.win32utils import Win32Utils
 
 
 class ShortcutKeys(object):
+    """This will provide generic common shortcut keys(combinations)
+    with Win32 API.
+
+    NOTICE: Most of the shortcut keys from Microsfot Windows Shortcut Keys.
+    May not work in Java Application.
+    """
+
     def __init__(self) -> None:
         self.logger = Logger("pyjab")
         self.win32_utils = Win32Utils()
         self.press_hold_release_key = self.win32_utils._press_hold_release_key
         self.press_key = self.win32_utils._press_key
 
-    # Common shortcut keys
-    def clear_text(self) -> None:
-        self.logger.debug("send shortcut command clear text")
-        self.press_key("home")
-        self.press_hold_release_key("ctrl", "shift", "end")
-        # TODO: need find out method to clear text for JABElement
-        for _ in range(100):
-            self.press_key("backspace")
+    def cut(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Cut'(Ctrl+X)")
+        self.win32_utils._press_hold_release_key("ctrl", "x")
+
+    def copy(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Copy'(Ctrl+C)")
+        self.win32_utils._press_hold_release_key("ctrl", "c")
 
     def paste(self) -> None:
-        self.logger.debug("paste text from clipboard")
-        self.press_hold_release_key("ctrl", "v")
+        self.logger.debug("Send Shortcut Key 'Paste'(Ctrl+V)")
+        self.win32_utils._press_hold_release_key("ctrl", "v")
 
-    # Oracle Form shortcut keys
-    def block_menu(self) -> None:
-        self.logger.debug("send shortcut command block menu")
-        self.press_hold_release_key("ctrl", "b")
+    def undo(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Undo'(Ctrl+Z)")
+        self.win32_utils._press_hold_release_key("ctrl", "z")
 
-    def clear_block(self) -> None:
-        self.logger.debug("send shortcut command clear block")
-        self.press_key("F7")
+    def popup_menu(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Popup Menu'(Alt+Space)")
+        self.win32_utils._press_hold_release_key("alt", "spacebar")
 
-    def clear_field(self) -> None:
-        self.logger.debug("send shortcut command clear field")
-        self.press_key("F5")
+    def back(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Back'(Alt+Left Arrow)")
+        self.win32_utils._press_hold_release_key("alt", "left_arrow")
 
-    def clear_form(self) -> None:
-        self.logger.debug("send shortcut command clear form")
-        self.press_key("F8")
+    def forward(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Forward'(Alt+Right Arrow)")
+        self.win32_utils._press_hold_release_key("alt", "right_arrow")
 
-    def clear_record(self) -> None:
-        self.logger.debug("send shortcut command clear record")
-        self.press_key("F6")
+    def move_screen_up(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Move Screen Up'(Alt+Page Up)")
+        self.win32_utils._press_hold_release_key("alt", "page_up")
 
-    def commit(self) -> None:
-        self.logger.debug("send shortcut command commit")
-        self.press_hold_release_key("ctrl", "s")
+    def move_screen_down(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Move Screen Down'(Alt+Page Down)")
+        self.win32_utils._press_hold_release_key("alt", "page_down")
 
-    def count_query(self) -> None:
-        self.logger.debug("send shortcut command count query")
-        self.press_key("F12")
+    def close_window(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Close Window'(Ctrl+F4)")
+        self.win32_utils._press_hold_release_key("ctrl", "f4")
 
-    def delete_record(self) -> None:
-        self.logger.debug("send shortcut command delete record")
-        self.press_hold_release_key("ctrl", "up_arrow")
+    def select_all(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Select All'(Ctrl+A)")
+        self.win32_utils._press_hold_release_key("ctrl", "a")
 
-    def display_error(self) -> None:
-        self.logger.debug("send shortcut command display error")
-        self.press_hold_release_key("shift", "ctrl", "e")
+    def delete(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Delete Selected'(Ctrl+D)")
+        self.win32_utils._press_hold_release_key("ctrl", "d")
 
-    def down(self) -> None:
-        self.logger.debug("send shortcut command down")
-        self.press_key("down_arrow")
+    def refresh(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Refresh'(Ctrl+R)")
+        self.win32_utils._press_hold_release_key("ctrl", "r")
 
-    def duplicate_field(self) -> None:
-        self.logger.debug("send shortcut command duplicate field")
-        self.press_hold_release_key("shift", "F5")
+    def redo(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Redo'(Ctrl+Y)")
+        self.win32_utils._press_hold_release_key("ctrl", "y")
 
-    def duplicate_record(self) -> None:
-        self.logger.debug("send shortcut command duplicate record")
-        self.press_hold_release_key("shift", "F6")
+    def to_next_word(self) -> None:
+        self.logger.debug(
+            "Send Shortcut Key 'Move Cursor to Next Word'(Ctrl+Right Arrow)"
+        )
+        self.win32_utils._press_hold_release_key("ctrl", "right_arrow")
 
-    def edit(self) -> None:
-        self.logger.debug("send shortcut command edit")
-        self.press_hold_release_key("ctrl", "e")
+    def to_previous_word(self) -> None:
+        self.logger.debug(
+            "Send Shortcut Key 'Move Cursor to Previous Word'(Ctrl+Left Arrow)"
+        )
+        self.win32_utils._press_hold_release_key("ctrl", "left_arrow")
 
-    def enter_query(self) -> None:
-        self.logger.debug("send shortcut command enter query")
-        self.press_key("F11")
+    def to_next_paragraph(self) -> None:
+        self.logger.debug(
+            "Send Shortcut Key 'Move Cursor to Next Paragraph'(Ctrl+Down Arrow)"
+        )
+        self.win32_utils._press_hold_release_key("ctrl", "down_arrow")
 
-    def execute_query(self) -> None:
-        self.logger.debug("send shortcut command execute query")
-        self.press_hold_release_key("ctrl", "F11")
+    def to_previous_paragraph(self) -> None:
+        self.logger.debug(
+            "Send Shortcut Key 'Move Cursor to Previous Paragraph'(Ctrl+Up Arrow)"
+        )
+        self.win32_utils._press_hold_release_key("ctrl", "up_arrow")
 
-    def _exit(self) -> None:
-        self.logger.debug("send shortcut command exit")
-        self.press_key("F4")
+    def to_end(self) -> None:
+        self.logger.debug(
+            "Send Shortcut Key 'Move Cursor to End of Document'(Ctrl+End)"
+        )
+        self.win32_utils._press_hold_release_key("ctrl", "end")
 
-    def _help(self) -> None:
-        self.logger.debug("send shortcut command help")
-        self.press_hold_release_key("ctrl", "h")
+    def to_home(self) -> None:
+        self.logger.debug(
+            "Send Shortcut Key 'Move Cursor to Home of Document'(Ctrl+Home)"
+        )
+        self.win32_utils._press_hold_release_key("ctrl", "home")
 
-    def insert_record(self) -> None:
-        self.logger.debug("send shortcut command insert record")
-        self.press_hold_release_key("ctrl", "down_arrow")
+    def select_to_end(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Select to End'(Ctrl+Shift+End)")
+        self.win32_utils._press_hold_release_key("ctrl", "shift", "end")
 
-    def list_of_values(self) -> None:
-        self.logger.debug("send shortcut command list of values")
-        self.press_hold_release_key("ctrl", "l")
-
-    def list_tab_pages(self) -> None:
-        self.logger.debug("send shortcut command list tab pages")
-        self.press_key("F2")
-
-    def next_block(self) -> None:
-        self.logger.debug("send shortcut command next block")
-        self.press_hold_release_key("shift", "page_down")
-
-    def next_field(self) -> None:
-        self.logger.debug("send shortcut command next field")
-        self.press_key("tab")
-
-    def next_primary_key(self) -> None:
-        self.logger.debug("send shortcut command next primary key")
-        self.press_hold_release_key("shift", "F7")
-
-    def next_record(self) -> None:
-        self.logger.debug("send shortcut command next record")
-        self.press_key("down_arrow")
-
-    def next_set_of_records(self) -> None:
-        self.logger.debug("send shortcut command next set of records")
-        self.press_key("shift", "F8")
-
-    def previous_block(self) -> None:
-        self.logger.debug("send shortcut command previous block")
-        self.press_key("shift", "page_up")
-
-    def previous_field(self) -> None:
-        self.logger.debug("send shortcut command previous field")
-        self.press_key("shift", "tab")
-
-    def previous_record(self) -> None:
-        self.logger.debug("send shortcut command previous record")
-        self.press_key("shift", "up_arrow")
-
-    def _print(self) -> None:
-        self.logger.debug("send shortcut command print")
-        self.press_key("ctrl", "p")
-
-    def _return(self) -> None:
-        self.logger.debug("send shortcut command return")
-        self.press_key("clear")
-
-    def scroll_down(self) -> None:
-        self.logger.debug("send shortcut command scroll down")
-        self.press_key("page_down")
-
-    def scroll_up(self) -> None:
-        self.logger.debug("send shortcut command scroll up")
-        self.press_key("page_up")
-
-    def show_keys(self) -> None:
-        self.logger.debug("send shortcut command show keys")
-        self.press_key("ctrl", "k")
-
-    def up(self) -> None:
-        self.logger.debug("send shortcut command up")
-        self.press_key("up_arrow")
-
-    def update_record(self) -> None:
-        self.logger.debug("send shortcut command update record")
-        self.press_key("ctrl", "u")
+    def select_to_home(self) -> None:
+        self.logger.debug("Send Shortcut Key 'Select to Home'(Ctrl+Shift+Home)")
+        self.win32_utils._press_hold_release_key("ctrl", "shift", "home")
